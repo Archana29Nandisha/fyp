62fdf26923584f876ff23d0c76f61452becad762
Archana Nandisha Udugampola <125017946+Archana29Nandisha@users.noreply.github.com>
2025-03-11 00:39:43 +0530

    Create multerMiddleware.js

---
+++ src/middlewares/multerMiddleware.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Storage setup
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// File filter (Allow only images)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed!"), false);
+  }
+};
+
+export const upload = multer({ storage, fileFilter });
